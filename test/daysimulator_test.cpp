4b061685c02fbf759959deff12dd9f480f71cfbb
Simon Egli <simon.egli@bbv.ch>
2016-03-29 21:10:28 +0200

    Rename test to reflect what it does
    Remove dummy name
    
    Signed-off-by: Simon Egli <simon.egli@bbv.ch>

--- test/daysimulator_test.cpp
+++ test/daysimulator_test.cpp
@@ -16,7 +16,7 @@ class DaySimulatorTest : public testing::Test {
 
 };
 
-TEST_F(DaySimulatorTest, can_Execute_Google_Test)  {
+TEST_F(DaySimulatorTest, valid_day_and_night_periods)  {
     for(int i=0;i<24;i++)
       EXPECT_EQ(day_->getTimeOfDay(i),Daytime::DAY);
     for(int i=35;i<60;i++)

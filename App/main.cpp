f9a000e66516d352fdd4e4305e521b850a12abb8
Simon Egli <simon.egli@bbv.ch>
2016-03-31 10:21:28 +0200

    Registering DaySimulator in Qml
    Signed-off-by: Simon Egli <simon.egli@bbv.ch>

--- App/main.cpp
+++ App/main.cpp
@@ -1,9 +1,15 @@
+#include "daysimulator.h"
+
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+        qmlRegisterType<DaySimulator>("com.bbv.StreetLightSimulator", 1, 0, "DaySimulator");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
